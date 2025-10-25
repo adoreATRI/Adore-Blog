3f26d44c07fec1ab31b411ae70d6e36deadd5932
ATRI <makochishima043@gmail.com>
2025-10-25 12:12:32 +0800

    添加vercel地址

--- src/config.ts
+++ src/config.ts
@@ -301,7 +301,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo.vercel.app",
+		envId: "https://my-twikoo-comment.vercel.app",
 		lang: "zh-CN", // 设置 Twikoo 评论系统语言为中文
 	},
 };
